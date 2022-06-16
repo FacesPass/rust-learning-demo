f899c5369578e5ec47c84baebfc479f70f639fe7
JiquanWang99 <951873902@qq.com>
2022-06-16 21:59:54 +0800

    feat: rust

--- 01.初练习/15.macro_rule_demo/src/main.rs
+++ 01.初练习/15.macro_rule_demo/src/main.rs
@@ -13,12 +13,44 @@ macro_rules! scanline {
     }};
 }
 
+macro_rules! find_min {
+    ($x: expr) => {
+        $x
+    };
+    ($x: expr, $($y: expr), +) => {
+        std::cmp::min($x, find_min!($($y), +))
+    };
+}
+
+macro_rules! calculate {
+    (eval $e: expr) => {{
+        {
+            let val: usize = $e;
+            println!("{} = {}",stringify!($e), val);
+        }
+    }};
+
+    (eval $e: expr, $(eval $x: expr), +) => {{
+        calculate! {eval $e}
+        calculate! {$(eval $x), +}
+    }};
+}
+
 fn main() {
-    println!("请输入第一个值:");
-    let mut input = String::new();
-    scanline!(input);
-    println!("Hi: {}", input);
-    println!("请输入第二个值:");
-    let a = scanline!();
-    println!("Hi: {}", a);
+    // println!("请输入第一个值:");
+    // let mut input = String::new();
+    // scanline!(input);
+    // println!("Hi: {}", input);
+    // println!("请输入第二个值:");
+    // let a = scanline!();
+    // println!("Hi: {}", a);
+
+    // let min = find_min!(1, 2, 3);
+    // println!("{}", &min);
+
+    calculate! {
+        eval 1 + 1,
+        eval 3 + 4,
+        eval (2 * 3) + 1
+    };
 }
